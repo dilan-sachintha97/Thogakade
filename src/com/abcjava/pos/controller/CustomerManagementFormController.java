6b3101678ef501a885cfa2493e94b3b7a37e3bb2
dilan-sachintha97 <dil97an@gmail.com>
2022-11-05 23:01:45 +0530

    4

--- src/com/abcjava/pos/controller/CustomerManagementFormController.java
+++ src/com/abcjava/pos/controller/CustomerManagementFormController.java
@@ -2,9 +2,11 @@ package com.abcjava.pos.controller;
 
 import com.abcjava.pos.db.Database;
 import com.abcjava.pos.modal.Customer;
+import com.abcjava.pos.view.tm.CustomerTm;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.scene.control.Alert;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 
 public class CustomerManagementFormController {
@@ -13,6 +15,28 @@ public class CustomerManagementFormController {
     public TextField txtName;
     public TextField txtAddress;
     public TextField txtSalary;
+    public TableView<CustomerTm> tblCustomerDetails;
+    public TableColumn colId;
+    public TableColumn colName;
+    public TableColumn colAddress;
+    public TableColumn colSalary;
+    public TableColumn colOptions;
+
+    public void initialize(){
+        searchCustomers();
+    }
+
+    private void searchCustomers(){
+        ObservableList<CustomerTm> tmList = FXCollections.observableArrayList();
+        for (Customer c : Database.customerList){
+            Button button = new Button("Delete");
+            CustomerTm customerTm = new CustomerTm(
+                    c.getId(),c.getName(),c.getAddress(),c.getSalary(),button
+            );
+            tmList.add(customerTm);
+        }
+        tblCustomerDetails.setItems(tmList);
+    }
 
     public void btnSaveCustomerOnAction(ActionEvent actionEvent) {
 
