98e8d1f58da51e9830916b2c5580cc6a3949d2e4
dilan-sachintha97 <dil97an@gmail.com>
2022-11-05 21:39:26 +0530

    2

--- src/com/abcjava/pos/controller/CustomerManagementFormController.java
+++ src/com/abcjava/pos/controller/CustomerManagementFormController.java
@@ -1,7 +1,17 @@
 package com.abcjava.pos.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
 public class CustomerManagementFormController {
     public AnchorPane customerManagementContext;
+    public TextField txtId;
+    public TextField txtName;
+    public TextField txtAddress;
+    public TextField txtSalary;
+
+    public void btnSaveCustomerOnAction(ActionEvent actionEvent) {
+    }
 }
+
---
+++ src/com/abcjava/pos/db/Database.java
@@ -0,0 +1,9 @@
+package com.abcjava.pos.db;
+
+import com.abcjava.pos.modal.Customer;
+
+import java.util.ArrayList;
+
+public class Database {
+    public static ArrayList<Customer> customerList = new ArrayList<Customer>();
+}
