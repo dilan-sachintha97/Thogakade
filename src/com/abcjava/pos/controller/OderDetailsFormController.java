f442d01dff80485e64489059f980408252f428da
dilan-sachintha97 <dil97an@gmail.com>
2022-11-17 10:39:33 +0530

    created oder details form

---
+++ src/com/abcjava/pos/controller/OderDetailsFormController.java
@@ -0,0 +1,18 @@
+package com.abcjava.pos.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class OderDetailsFormController {
+
+    public TableView tblOderDetails;
+    public TableColumn colOderId;
+    public TableColumn colCustomer;
+    public TableColumn colDate;
+    public TableColumn colTotal;
+    public TableColumn colOptions;
+
+    public void btnBackToHomeOnAction(ActionEvent actionEvent) {
+    }
+}
